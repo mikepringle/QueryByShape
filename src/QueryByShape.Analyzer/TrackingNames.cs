94419bac38ba0fa47b548d8427244a63d3a3e7cb
Mike Pringle <mikepringle@hotmail.com>
2025-12-03 13:09:04 -0500

    Test Caching

---
+++ src/QueryByShape.Analyzer/TrackingNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryByShape.Analyzer
+{
+    internal class TrackingNames
+    {
+        public const string Symbols = nameof(Symbols);
+        public const string QueryDeclarations = nameof(QueryDeclarations);
+        public const string QueryHierarchy = nameof(QueryHierarchy);
+    }
+}
